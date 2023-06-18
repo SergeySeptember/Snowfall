cf7d8098e8cf478c728b7f159a6c4b06f732c08f
Сергей Мешков <sergey_meshkov.ru@mail.ru>
2023-06-18 22:52:45 +0300

    [NF] Added the function of saving data in a json file and loading data from json when there is no internet connection

--- Snowfall/Form1.cs
+++ Snowfall/Form1.cs
@@ -5,10 +5,13 @@ namespace Snowfall
 {
     public partial class Form1 : Form, INotifyPropertyChanged
     {
-        private GoogleHelper helper;
+        private GoogleHelper _helper;
         string[] token = File.ReadAllLines(@"C:\Users\September\Desktop\credentials.json");
         string todoSheet = "Example";
-
+        BindingList<TaskBody> listOfTusks = new BindingList<TaskBody>();
+        string path = $"{Environment.CurrentDirectory}\\ListOfTasks.json";
+        private FileIOService _fileIOService;
+        bool successConnect;
         private BindingList<TaskBody> _taskBody;
 
         public BindingList<TaskBody> TaskBodyProperty
@@ -26,71 +29,119 @@ namespace Snowfall
             InitializeComponent();
         }
 
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            StartConnect();
-
             dataGridView1.DataBindings.Add("DataSource", this, nameof(TaskBodyProperty));
             TaskBodyProperty = new BindingList<TaskBody>();
 
-            InitialLoadTask();
-            ColumnsConfig();
+            StartConnect();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-
         private async void StartConnect()
         {
-            this.helper = new GoogleHelper(token[0], todoSheet);
+            this._helper = new GoogleHelper(token[0], todoSheet);
 
-            bool success = this.helper.Start().Result;
+            try
+            {
+                successConnect = this._helper.Start().Result;
+            }
+            catch (Exception)
+            {
+                successConnect = false;
+            }
+
+            if (successConnect == true)
+            {
+                LoadDataFromGDrive();
+            }
+            else
+            {
+                LoadDataFromJson();
+            }
+            ColumnsConfig();
         }
 
         private void ColumnsConfig()
         {
-            dataGridView1.Columns[0].Width = 300;
+            dataGridView1.Columns[0].Width = 100;
         }
 
-        private int ExcelNemberOfRaws()
+        private void LoadDataFromGDrive()
         {
-            int countOfRaw = this.helper.GetCountOfRaws(cellName: "A", cellName2: "A");
-            return countOfRaw;
-        }
-
-        private void InitialLoadTask()
-        {
-            int countOfRaw = ExcelNemberOfRaws();
+            int countOfRaw = NemberOfRawsOnGDrive();
             if (countOfRaw != 0)
             {
                 for (int i = 1; i <= countOfRaw; i++)
                 {
-                    TaskBody result = this.helper.Get(cellName: $"A{i}", cellName2: $"D{i}");
-                    TaskBodyProperty.Add(result);
+                    TaskBody result = this._helper.Get(cellName: $"A{i}", cellName2: $"D{i}");
+                    listOfTusks.Add(result);
                 }
 
+                for (int i = 0; i < countOfRaw; i++)
+                {
+                    TaskBodyProperty.Add(listOfTusks[i]);
+                }
             }
         }
 
-        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        private int NemberOfRawsOnGDrive()
+        {
+            int countOfRaw = this._helper.GetCountOfRaws(cellName: "A", cellName2: "A");
+            return countOfRaw;
+        }
+
+        private void LoadDataFromJson()
+        {
+            listOfTusks = FileIOService.LoadData(path);
+            int count = listOfTusks.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                TaskBodyProperty.Add(listOfTusks[i]);
+            }
+        }
+
+        private void UpdateListTask()
         {
             int lineNumber = dataGridView1.CurrentCell.RowIndex + 1;
-            var taskBody = TaskBodyProperty[lineNumber - 1];
+            int index = lineNumber - 1;
+            var currentTaskBody = TaskBodyProperty[index];
 
-            this.helper.Set(cellName1: $"A{lineNumber}", cellName2: $"D{lineNumber}", taskBody.Task, taskBody.Status.ToString(), taskBody.Category, taskBody.Time);
+            if (listOfTusks.Count < index)
+            {
+                listOfTusks[index].Task = currentTaskBody.Task;
+                listOfTusks[index].Status = currentTaskBody.Status;
+                listOfTusks[index].Category = currentTaskBody.Category;
+                listOfTusks[index].Time = currentTaskBody.Time;
+            }
+            else
+            {
+                listOfTusks.Add(currentTaskBody);
+            }
+
+            FileIOService.SaveData(listOfTusks, path);
         }
 
-        private void buttonSet_Click(object sender, EventArgs e)
+        private void WriteDataToGoogle()
         {
-            //
+            int lineNumber = dataGridView1.CurrentCell.RowIndex + 1;
+            int index = lineNumber - 1;
+            var taskBody = TaskBodyProperty[index];
+
+            this._helper.Set(cellName1: $"A{lineNumber}", cellName2: $"D{lineNumber}", taskBody.Task, taskBody.Status.ToString(), taskBody.Category, taskBody.Time);
         }
 
-        private void buttonGet_Click(object sender, EventArgs e)
+        //Handler
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-            //
+            UpdateListTask();
+
+            if (successConnect == true)
+                WriteDataToGoogle();
         }
 
     }
